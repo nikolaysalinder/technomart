fda0a0c09b9daf8b1539d0c80e838eaf99fae6c7
nikolaysalinder <nikolay.salinder@gmail.com>
2018-03-07 04:05:43 +0500

    ended main footer without states

---
+++ src/App.vue
@@ -0,0 +1,163 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <main>
+      <div class="container promo">
+        <div class="promo-item">
+          <h2 class="promo-item-title">Матералы</h2>
+          <a href="" class="promo-item-info">На любой вкус</a>
+        </div>
+        <div class="promo-item">
+          <h2 class="promo-item-title">Иструмент</h2>
+          <a href="" class="promo-item-info">На все случаи</a>
+        </div>
+        <div class="promo-item">
+          <h2 class="promo-item-title">Техника</h2>
+          <a href="" class="promo-item-info">Для стройки</a>
+        </div>
+        <div class="promo-slider">
+        </div>
+        <div class="promo-item">
+          <h2 class="promo-item-title">Скидка 50%</h2>
+          <a href="" class="promo-item-info">На 350 товаров</a>
+        </div>
+        <div class="promo-item">
+          <h2 class="promo-item-title">Доставка</h2>
+          <a href="" class="promo-item-info">Бесплатно</a>
+        </div>
+      </div>
+      <div class="goods">
+        <header class="section-header">
+          <h3 >Популярные товары</h3>
+          <a href="#">Все популярные товары</a>
+        </header>
+        <div class="goods-item">
+          <div class="flag flag-new">Новинка</div>
+          <div class="actions">
+            <a href="#" class="buy">Купить</a>
+            <a href="#" class="bookmark">В закладки</a>
+          </div>
+          <div class="goods-image">
+            <a href="" class="brands-logo">
+              <img
+              src="img/brands-logo-1.jpg"
+              alt="перфоратор Bosh BFG 3000"
+              width="218"
+              height="170">
+            </a>
+          </div>
+          <h3
+          class="goods-title">Перфоратор Bosch bfg 3000</h3>
+          <div class="discount">22 500 Р.</div>
+          <div class="price">15 500 Р.</div>
+        </div>
+      </div>
+      <div class="brands">
+        <header class="section-header">
+          <h3 >Популярные производители</h3>
+          <a href="#">Все производители</a>
+        </header>
+        <div class="brands-item">
+          <img src="#" alt="#">
+        </div>
+      </div>
+      <div class="services">
+        <h3 class="services-title">Сервисы</h3>
+        <p class="services-info">
+          Хороший интернет-магазин отличается от плохого не только ценами!
+        </p>
+        <p class="service-info">
+          Мы стараемся изо всех сил чтобы сделать ваши покупки приятными.
+        </p>
+        <ul class="services-column-left">
+          <li><a href="#">Доставка</a></li>
+          <li><a href="#">Гарантия</a></li>
+          <li><a href="#">Кредит</a></li>
+        </ul>
+        <ul class="services-column-right">
+          <h3 class="services-item-title"></h3>
+          <p class="services-item-info"></p>
+          <img src="#" alt="#">
+        </ul>
+      </div>
+      <div class="about">
+        <div class="about-column-left">
+          <h3 class="title-about">Компания Техномарт</h3>
+          <p>tre man shopping</p>
+          <p>WE can</p>
+          <ul class="partners">
+            <li>Деловые линии</li>
+            <li>Деловые линии</li>
+            <li>Деловые линии</li>
+          </ul>
+          <a href="#" class="btn btn-about">Подробнее о компании</a>
+        </div>
+        <div class="about-column-right">
+          <h3 class="title-about">Контакты</h3>
+          <p>Вы сможете забрать товар</p>
+          <div id="about-map"></div>
+          <button class="btn btn-map">Заблудились? Напишите нам!</button>
+        </div>
+      </div>
+    </main>
+    <footer class="main-footer">
+      <section class="top-footer">
+        <a href="#" class="footer-logo">Техномарт</a>
+        <nav class="footer-nav-top">
+          <ul>
+            <li><a href="#">Компания</a></li>
+            <li><a href="#">Новости</a></li>
+            <li><a href="#">Каталог</a></li>
+            <li><a href="#">Доставка</a></li>
+            <li><a href="#">Контакты</a></li>
+          </ul>
+        </nav>
+        <p>г. Санкт-Петербург.</p>
+        <nav class="footer-nav-bottom">
+          <ul>
+            <li><a href="#">Компания</a></li>
+            <li><a href="#">Новости</a></li>
+            <li><a href="#">Каталог</a></li>
+            <li><a href="#">Доставка</a></li>
+            <li><a href="#">Контакты</a></li>
+          </ul>
+        </nav>
+      </section>
+      <section class="footer-bottom">
+        <div class="footer-copyright">
+          2010-1017
+        </div>
+        <div class="footer-socials">
+          <a href="">vk</a>
+          <a href="">fb</a>
+          <a href="">inst</a>
+        </div>
+        <div class="footer-developer">
+          <ul>
+            <li>
+              <span>Обратная связь:</span>
+              <a href="">mail@nikolaysalinder.ru</a>
+            </li>
+            <li>
+              <span>Разработано - </span>
+              <a href="">nikolaysalinder.ru</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Cuprum', 'PT Sans', sans-serif;
+  color: #000;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,273 @@
+<template>
+  <header class="main-header">
+    <div class="header-panel-top">
+      <div class="container">
+        <a href="/" class="header-logo">Техномарт</a>
+        <form action="#">
+          <label for="search">
+            <input id="search" type="text" placeholder="Поиск:" required>
+          </label>
+        </form>
+        <a href="#" class="bookmark">Закладки: 0</a>
+        <a href="#" class="cart">Корзина: 0</a>
+        <a href="#" class="order">Оформить заказ</a>
+      </div>
+    </div>
+    <div class="header-panel-middle">
+      <div class="container">
+        <p class="description">
+          Интернет-магазин<br>
+          строительных материалов<br>
+          и инструментов для ремонта<br>
+        </p>
+        <div class="contacts">
+          <a href="#" class="phone">+7 (812) 555-05-55</a>
+          <p class="adress">г. Санкт-Петербург, ул. Б. Конюшенная, д. 19/8</p>
+        </div>
+        <div class="user-block">
+          <a href="#" class="enter">Войти</a>
+          <a href="#" class="register">Регистрация</a>
+        </div>
+      </div>
+    </div>
+    <div class="header-panel-bottom">
+      <div class="container">
+        <nav class="main-navigation">
+          <ul>
+            <li><a href="#">Главная</a></li>
+            <li><a href="#">Компания</a></li>
+            <li><a href="#">Каталог</a></li>
+            <li><a href="#">Новости</a></li>
+            <li><a href="#">Спецпредложения</a></li>
+            <li><a href="#">Доставка</a></li>
+            <li><a href="#">Контакты</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+/*стили для разработки*/
+/*a, div, li, nav, ul, header,
+footer, figure, form,
+label, h1, h2, p, fieldset, button, input, section {
+  outline: 1px solid green;
+}*/
+/*стили для разработки*/
+body {
+  margin: 0;
+  padding: 0;
+}
+.container {
+  display: flex;
+  width: 940px;
+  padding: 0 10px;
+  margin: 0 auto;
+}
+.header-panel-top {
+  display: block;
+  margin: 0;
+  padding: 0;
+  background: #29344a;
+}
+.header-panel-top .header-logo {
+  display: block;
+  background: #ea3544;
+  color: #fff;
+  font-size: 24px;
+  line-height: 26px;
+  text-decoration: none;
+  text-transform: uppercase;
+  padding: 10px 55px;
+  padding-bottom: 5px;
+  margin: 0;
+  box-shadow: 0 4px  #b62a32;
+}
+.header-panel-top form {
+  display: flex;
+  width: 270px;
+  margin: 0;
+  padding: 0;
+}
+.header-panel-top label[for="search"] {
+  display: block;
+  margin: 0;
+  padding: 0;
+  height: 43px;
+  background: transparent;
+  width: 100%;
+  position: relative;
+}
+.header-panel-top label[for="search"]::before {
+  content: '';
+  position: absolute;
+  top: 12px;
+  left: 17px;
+  width: 20px;
+  height: 20px;
+  background: red;
+  background: url('./../assets/img/icon-search.png');
+  background-repeat: no-repeat;
+}
+.header-panel-top #search {
+  margin: 0;
+  border: none;
+  background: transparent;
+  width: 222px;
+  font-family: inherit;
+  font-size: 17px;
+  padding-left: 48px;
+  padding-top: 11px;
+  padding-bottom: 13px;
+  transition:  background .5s;
+}
+.header-panel-top #search:focus {
+  background: #fff;
+
+}
+.header-panel-top #search::placeholder {
+  color: #fff;
+}
+.header-panel-top #search:focus::placeholder {
+  color: #000;
+}
+.header-panel-top a {
+  display: block;
+  padding-top: 11px;
+  padding-left: 45px;
+  padding-bottom: 13px;
+  color: #fff;
+  text-decoration: none;
+}
+.header-panel-top .bookmark {
+  padding-right: 31px;
+  font-size: 17px;
+  background: url('./../assets/img/icon-bookmark.png');
+  background-repeat: no-repeat;
+  background-position: 17px 12px;
+}
+.header-panel-top .cart {
+  padding-right: 31px;
+  font-size: 17px;
+  background: url('./../assets/img/cart-icon.png');
+  background-repeat: no-repeat;
+  background-position: 14px 12px;
+}
+.header-panel-top .order {
+  padding-left: 20px;
+  font-size: 17px;
+  padding-right: 22px;
+  background: #63a63f;
+}
+.header-panel-middle {
+  padding-top: 32px;
+  background: #f3f7fa;
+}
+.header-panel-middle .description {
+  margin-left: 53px;
+  font-style: italic;
+  line-height: 23px;
+  color: #ea3644;
+}
+.header-panel-middle .phone {
+  display: block;
+  margin-top: 14px;
+  margin-left: 70px;
+  font-size: 21px;
+  padding-top: 5px;
+  padding-left: 70px;
+  padding-right: 49px;
+  padding-bottom: 8px;
+  border: 4px solid #fff;
+  text-decoration: none;
+  background: url('./../assets/img/icon-phone.png');
+  background-repeat: no-repeat;
+  background-position: 12px 8px;
+  color: #ea3644;
+}
+.header-panel-middle .adress {
+  font-size: 14px;
+  margin-top: 6px;
+  margin-left: 80px;
+  color: #303c54;
+}
+.header-panel-middle .user-block {
+  display: flex;
+  justify-content: space-between;
+  margin-left: 50px;
+  margin-top: 13px;
+  width: 300px;
+  height: 45px;
+}
+.header-panel-middle .enter {
+  display: block;
+  margin-left: 20px;
+  padding-top: 10px;
+  padding-left: 47px;
+  padding-bottom: 12px;
+  width: 75px;
+  color: #000;
+  font-size: 21px;
+  text-decoration: none;
+  background: #fff url('./../assets/img/icon-login.png');
+  background-repeat: no-repeat;
+  background-position: 14px 14px;
+}
+.header-panel-middle .register {
+  display: block;
+  float: right;
+  padding-top: 10px;
+  padding-right: 24px;
+  padding-bottom: 12px;
+  padding-left: 25px;
+  background: #fff;
+  color: #000;
+  font-size: 21px;
+  text-decoration: none;
+}
+.header-panel-bottom {
+  background: #f3f7fa;
+  padding-bottom: 59px;
+}
+.header-panel-bottom ul,
+.haeder-panel-bottom li {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.main-navigation {
+  margin-top: 24px;
+}
+.main-navigation ul {
+  display: flex;
+  width: 940px;
+  border-bottom: 5px solid #293448;
+}
+.main-navigation a {
+  display: block;
+  padding-left: 32px;
+  padding-right: 25px;
+  padding-top: 18px;
+  padding-bottom: 17px;
+  text-decoration: none;
+  font-size: 18px;
+  background: #32425c;
+  color: #fff;
+}
+.main-navigation a:hover {
+  background: #293448;
+}
+.main-navigation li:first-child a {
+  padding-left: 50px;
+}
+.main-navigation li:last-child a {
+  padding-right: 53px;
+}
+</style>
